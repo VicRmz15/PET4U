b7311fc770bce47ee28785c4d06efeb9680aad63
AA <>
2024-02-15 21:23:22 -0600

    Create MVVM Login

---
+++ Login/LoginCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  LoginCoordinator.swift
+//  PET4U
+//
+//  Created by Angel Tekiani Duarte Estrada on 15/02/24.
+//
+
+import Foundation
---
+++ Login/LoginViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  LoginViewModel.swift
+//  PET4U
+//
+//  Created by Angel Tekiani Duarte Estrada on 15/02/24.
+//
+
+import Foundation
